714e51f8f833422111aa190fa16343c8339dcc0d
FireCon <89361575+Ruben-Jim@users.noreply.github.com>
2025-03-22 21:13:57 -0700

    fixing new projects

--- src/components/NewProjects.jsx
+++ src/components/NewProjects.jsx
@@ -32,7 +32,7 @@ const NewProjects = () => {
 
   return (
     <div className='container mx-auto py-4 pt-20 px-6 md:px-30 lg:px-12 my-20 w-full overflow-hidden' id='Homes'>
-      <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Homes for Sale <span className='underline underline-offset-4 decoration-1 under font-light'>Completed</span></h1>
+      <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Homes <span className='underline underline-offset-4 decoration-1 under font-light'>for Sale</span></h1>
       <p className='text-center text-gray-500 mb-8 max-w-80 mx-auto'>Crafting Spaces, Building Legacies - Explore Our Portfolio</p>
 
       {/* slider buttons */}
